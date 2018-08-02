1ba0a50229a8253f284d5e40f787ba50f58e4280
Telolahy <stephano.telolahy@gmail.com>
2018-08-02 07:14:24 +0200

    add menu item

--- app/src/main/java/com/creativegames/rippledemo/MainActivity.kt
+++ app/src/main/java/com/creativegames/rippledemo/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.creativegames.rippledemo
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
     }
 }
